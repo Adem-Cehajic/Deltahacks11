4a9bacf9fd9f119168133bbf03a8fd4b870e4304
Aiden Ly <atl.aidenly@gmail.com>
2025-01-11 13:26:17 -0500

    Setting up Swift

---
+++ placeholder/placeholder/placeholderApp.swift
@@ -0,0 +1,17 @@
+//
+//  placeholderApp.swift
+//  placeholder
+//
+//  Created by Aiden Ly on 2025-01-11.
+//
+
+import SwiftUI
+
+@main
+struct placeholderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
