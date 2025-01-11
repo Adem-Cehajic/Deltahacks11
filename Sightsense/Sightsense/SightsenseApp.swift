94664b7a43343d683ebe71aed94fb3516f770f15
Aiden Ly <atl.aidenly@gmail.com>
2025-01-11 14:38:51 -0500

    remade project

---
+++ Sightsense/Sightsense/SightsenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightsenseApp.swift
+//  Sightsense
+//
+//  Created by Aiden Ly on 2025-01-11.
+//
+
+import SwiftUI
+
+@main
+struct SightsenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
