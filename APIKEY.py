cc767a4a7b8a2f5cbaaef730324ca06e4ef05102
KhushP27 <khushp2006@gmail.com>
2025-01-12 01:29:42 -0500

    APIKEY.py

---
+++ APIKEY.py
@@ -0,0 +1,1 @@
+api_key = "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"  # Replace with your actual OpenAI API key
